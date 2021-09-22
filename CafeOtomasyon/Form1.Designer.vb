63e9714763eb86bcb45ff919a94b9f4e8c672630
Onur Utku TOPALOĞLU <topaloglu08@gmail.com>
2021-09-22 16:29:27 +0300

    Proje dosyası ekle.

---
+++ CafeOtomasyon/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.portlar_ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.boudrate_ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.sayi_Label3 = New System.Windows.Forms.Label()
+        Me.okunan_Label4 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'portlar_ComboBox1
+        '
+        Me.portlar_ComboBox1.FormattingEnabled = True
+        Me.portlar_ComboBox1.Location = New System.Drawing.Point(7, 7)
+        Me.portlar_ComboBox1.Name = "portlar_ComboBox1"
+        Me.portlar_ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.portlar_ComboBox1.TabIndex = 1
+        Me.portlar_ComboBox1.Text = "Serial Port Seç..."
+        '
+        'boudrate_ComboBox2
+        '
+        Me.boudrate_ComboBox2.FormattingEnabled = True
+        Me.boudrate_ComboBox2.Items.AddRange(New Object() {"110", "300", "600", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200", "128000", "256000"})
+        Me.boudrate_ComboBox2.Location = New System.Drawing.Point(7, 34)
+        Me.boudrate_ComboBox2.Name = "boudrate_ComboBox2"
+        Me.boudrate_ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.boudrate_ComboBox2.TabIndex = 4
+        Me.boudrate_ComboBox2.Text = "BoudRate Seç..."
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(7, 62)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(121, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Bağlan"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'sayi_Label3
+        '
+        Me.sayi_Label3.AutoSize = True
+        Me.sayi_Label3.Location = New System.Drawing.Point(134, 67)
+        Me.sayi_Label3.Name = "sayi_Label3"
+        Me.sayi_Label3.Size = New System.Drawing.Size(13, 13)
+        Me.sayi_Label3.TabIndex = 7
+        Me.sayi_Label3.Text = "0"
+        '
+        'okunan_Label4
+        '
+        Me.okunan_Label4.AutoSize = True
+        Me.okunan_Label4.Location = New System.Drawing.Point(191, 67)
+        Me.okunan_Label4.Name = "okunan_Label4"
+        Me.okunan_Label4.Size = New System.Drawing.Size(76, 13)
+        Me.okunan_Label4.TabIndex = 8
+        Me.okunan_Label4.Text = "Kişi Ziyaret Etti"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(939, 607)
+        Me.Controls.Add(Me.okunan_Label4)
+        Me.Controls.Add(Me.sayi_Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.boudrate_ComboBox2)
+        Me.Controls.Add(Me.portlar_ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "G&O"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents portlar_ComboBox1 As ComboBox
+    Friend WithEvents boudrate_ComboBox2 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents sayi_Label3 As Label
+    Friend WithEvents okunan_Label4 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ CafeOtomasyon/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim portlar = System.IO.Ports.SerialPort.GetPortNames()
+        For Each port In portlar
+            Try
+                portlar_ComboBox1.Items.Clear()
+                portlar_ComboBox1.Items.Add(port)
+            Catch ex As Exception
+            End Try
+        Next
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim portlar = System.IO.Ports.SerialPort.GetPortNames()
+        For Each port In portlar
+            Try
+                portlar_ComboBox1.Items.Clear()
+                portlar_ComboBox1.Items.Add(port)
+            Catch ex As Exception
+            End Try
+        Next
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        SerialPort1.PortName = portlar_ComboBox1.Text
+        SerialPort1.BaudRate = boudrate_ComboBox2.Text
+        SerialPort1.Open()
+        Dim okunan = SerialPort1.ReadLine()
+        If okunan = 1 Then
+            Dim sonuc = sayi_Label3.Text + 1
+            sayi_Label3.Text = sonuc
+        End If
+        SerialPort1.Close()
+    End Sub
+End Class
